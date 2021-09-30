f934990962787d77f9a432a3c76a2b68463a4039
AaronTu <ken888686@gmail.com>
2021-09-30 18:36:40 +0800

    event target

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,27 +1,13 @@
-const title = document.querySelectorAll("h1");
-let count = 0;
-
-const btnPlus = document.querySelector(".btn-plus");
-btnPlus.addEventListener("click", (e) => {
-  count++;
-  title.forEach((element) => {
-    element.textContent = count;
-  });
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", (e) => {
+  console.log(e.target);
 });
 
-const btnMinus = document.querySelector(".btn-minus");
-btnMinus.addEventListener("click", () => {
-  count--;
-  title.forEach((element) => {
-    element.textContent = count;
-  });
+const list = document.querySelector(".list");
+list.addEventListener("click", (e) => {
+  // console.log(e.target.innerHTML);
+  // console.log(e.target.nodeName);
+  if (e.target.nodeName == "INPUT") {
+    console.log("Button!!!");
+  }
 });
-
-const btnClick = document.querySelector(".btn-click");
-btnClick.addEventListener("click", (event) => {
-  console.log(event);
-  console.log(event.clientX);
-});
-
-// 可查詢目前的標籤
-console.log(btnClick.nodeName);
