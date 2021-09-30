2fc868caca9aca6fc8e6096701b5cf2142ea4e28
g6520 <aaron.tu@evertrust.com.tw>
2021-09-30 17:01:23 +0800

    getAttribute

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,5 @@
-// querySelector: 找到第一個就停了
-// querySelectorAll: 會選擇所有相同的標籤
-
-const myLink = document.querySelectorAll("a");
-console.log(myLink);
-myLink[1].setAttribute("class", "red");
+const link = document.querySelector("a");
+console.log(link.getAttribute("href"));
+console.log(link.getAttribute("class"));
+console.log(document.querySelector(".main").innerHTML);
+console.log(document.querySelector(".main").textContent);
