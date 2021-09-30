30f9c8b3b9850252b99fb0dbb3dc363dc39b2483
g6520 <aaron.tu@evertrust.com.tw>
2021-09-30 17:12:05 +0800

    表單取值

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,5 +1,6 @@
-const link = document.querySelector("a");
-console.log(link.getAttribute("href"));
-console.log(link.getAttribute("class"));
-console.log(document.querySelector(".main").innerHTML);
-console.log(document.querySelector(".main").textContent);
+const txt = document.querySelector(".txt");
+console.log(txt.value);
+
+const list = document.querySelector(".list");
+list.value = "B";
+console.log(list.value);
