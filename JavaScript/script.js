86fd89f4737f75275c518e0d12255a712de516b7
AaronTu <ken888686@gmail.com>
2021-09-30 22:15:44 +0800

    filter

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,7 +1,54 @@
-let data = [1, 2, 3, 4, 5, 6, 7];
-let result = 0;
+let data = [
+  {
+    charger: "free",
+    name: "A",
+  },
+  {
+    charger: "free",
+    name: "B",
+  },
+  {
+    charger: "coin",
+    name: "C",
+  },
+  {
+    charger: "coin",
+    name: "D",
+  },
+  {
+    charger: "free",
+    name: "E",
+  },
+];
 
-data.forEach((item, index, arr) => {
-  console.log(`item: ${item}, index: ${index}, array: ${arr}`);
+const list = document.querySelector(".list");
+const stationFilter = document.querySelector(".filter");
+
+function init() {
+  let result = "";
+  data.forEach((x) => {
+    result += `<li>${x.name}: ${x.charger}</li>`;
+  });
+  list.innerHTML = result;
+}
+
+init();
+
+stationFilter.addEventListener("click", (e) => {
+  if (e.target.value == undefined) {
+    return;
+  }
+
+  if (e.target.value === "all") {
+    init();
+    return;
+  } else {
+    let result = "";
+    data.forEach((x) => {
+      if (e.target.value === x.charger) {
+        result += `<li>${x.name}: ${x.charger}</li>`;
+      }
+    });
+    list.innerHTML = result;
+  }
 });
-console.log(`hello`);
