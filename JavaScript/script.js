c17e4df220e92c7dae24f777e06e8820b9d6dbe5
g6520 <aaron.tu@evertrust.com.tw>
2021-09-30 15:19:07 +0800

    push/unshift

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,9 @@
-// null: 有被賦予值，但為空值
-let a = 1;
-console.log(typeof a);
+// push: 加在後面
+// unshift: 加在前面
+
+let x = [1, 2, "as"];
+x.push("qwer");
+console.log(x);
+
+x.unshift("asdf");
+console.log(x);
