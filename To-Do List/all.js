5964c9338ac65cd8008e43cc8937d7d63239cb81
g6520 <aaron.tu@evertrust.com.tw>
2021-10-05 11:47:29 +0800

    delete/done/undo

--- To-Do List/all.js
+++ To-Do List/all.js
@@ -13,19 +13,32 @@ let data = [
   },
 ];
 
+let done = [
+  {
+    content: "Done",
+  },
+];
+
 const list = document.querySelector(".list");
+const doneList = document.querySelector(".done-list");
 const txt = document.querySelector(".txt");
 const save = document.querySelector(".save");
 
 // 顯示現有事項
-function renderData(data) {
+function renderData() {
   let str = "";
   data.forEach(function (value, index) {
-    str += `<li key="${index}">${value.content} <input type="button" class="delete"value="Delete"></li>`;
+    str += `<li key="${index}">${value.content} <input type="button" data-num=${index} class="done" value="Done"><input type="button" data-num=${index} class="delete" value="Delete"></li>`;
   });
   list.innerHTML = str;
+
+  str = "";
+  done.forEach(function (value, index) {
+    str += `<li>${value.content} <input type="button" data-num="${index}" class="undo" value="Undo"><input type="button" data-num="${index}" class="delete" value="Delete"></li>`;
+  });
+  doneList.innerHTML = str;
 }
-renderData(data);
+renderData();
 
 // Save
 save.addEventListener("click", function (e) {
@@ -37,6 +50,53 @@ save.addEventListener("click", function (e) {
   let obj = {};
   obj.content = txt.value;
   data.push(obj);
-  renderData(data);
+  renderData();
   txt.value = "";
+});
+
+// Delete
+list.addEventListener("click", function (e) {
+  if (e.target.getAttribute("class") !== "delete") {
+    console.warn("Click Delete");
+    return;
+  }
+  let num = e.target.getAttribute("data-num");
+  data.splice(num, 1);
+  renderData();
+});
+
+doneList.addEventListener("click", function (e) {
+  if (e.target.getAttribute("class") !== "delete") {
+    console.warn("Click Delete");
+    return;
+  }
+  let num = e.target.getAttribute("data-num");
+  done.splice(num, 1);
+  renderData();
+});
+
+// Done
+list.addEventListener("click", function (e) {
+  if (e.target.getAttribute("class") !== "done") {
+    return;
+  }
+
+  let num = e.target.getAttribute("data-num");
+  done.push(data[num]);
+  data.splice(num, 1);
+
+  renderData();
+});
+
+//Undo
+doneList.addEventListener("click", function (e) {
+  if (e.target.getAttribute("class") !== "undo") {
+    return;
+  }
+
+  let num = e.target.getAttribute("data-num");
+  data.push(done[num]);
+  done.splice(num, 1);
+
+  renderData();
 });
