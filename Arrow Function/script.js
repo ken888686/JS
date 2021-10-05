ed8cb5df825d80ce09e3a0f12ea76f022f7a5e6d
g6520 <aaron.tu@evertrust.com.tw>
2021-10-05 13:23:13 +0800

    array map/filter

--- Arrow Function/script.js
+++ Arrow Function/script.js
@@ -1,10 +1,17 @@
-const num = (x) => {
-  return x * x;
-};
-console.log(num(2));
+let data = [1, 2, 3, 4, 5, 6];
+console.log(data);
 
-const num2 = (x) => x * x;
-console.log(num2(4));
+const newData = data.map(function (item) {
+  return item + 3;
+});
+console.log(newData);
 
-const num3 = () => 8;
-console.log(num3());
+const newData2 = data.map((item) => {
+  return item + 3;
+});
+console.log(newData2);
+
+const newData3 = data.map((item) => item + 3);
+console.log(newData3);
+
+console.log(data.filter((x) => x > 2 && x % 2 == 0));
