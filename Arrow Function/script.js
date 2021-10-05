86aabb780ed5c5d328955f60df162438543abde1
g6520 <aaron.tu@evertrust.com.tw>
2021-10-05 13:03:32 +0800

    arrow function縮寫

--- Arrow Function/script.js
+++ Arrow Function/script.js
@@ -1,19 +1,10 @@
-let x = 100;
-
-console.log(A(5));
-// 函式陳述式
-function A(x) {
-  return x * x;
-}
-
-// 函式表達式
-const B = function (x) {
+const num = (x) => {
   return x * x;
 };
-console.log(B(5));
+console.log(num(2));
 
-// 箭頭函式
-const C = (x, y) => {
-  return x * x * y;
-};
-console.log(C(5));
+const num2 = (x) => x * x;
+console.log(num2(4));
+
+const num3 = () => 8;
+console.log(num3());
